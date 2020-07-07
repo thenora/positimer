173c4cb1042cfce907d6f17d3f422e80a03e4fa1
thenora <thenora@gmail.com>
2020-07-06 18:08:42 -0700

    formatted

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Work = () => {
-  const [workTime, setWorkTime] = useState(300) // time in seconds
+  const [workTime, setWorkTime] = useState(300); // time in seconds
 
   const lowerWorkTimeByOneMinute = () => {
     const newWorkTime = workTime - 60;
 
-    if (newWorkTime <0) {
+    if (newWorkTime < 0) {
       setWorkTime(0);
     } else {
       setWorkTime(newWorkTime);
@@ -19,16 +19,16 @@ const Work = () => {
 
   return (
     <div>
-      <p id="work-label">
-        Work
-      </p>
-      <p id="work-time">
-        {workTime}
-      </p>
-      <button id="work-lower" onClick={lowerWorkTimeByOneMinute}>-</button>
-      <button id="work-raise" onClick={raiseWorkTimeByOneMinute}>+</button>
+      <p id="work-label">Work</p>
+      <p id="work-time">{workTime}</p>
+      <button id="work-lower" onClick={lowerWorkTimeByOneMinute}>
+        -
+      </button>
+      <button id="work-raise" onClick={raiseWorkTimeByOneMinute}>
+        +
+      </button>
     </div>
   );
-}
+};
 
-export default Work
+export default Work;
