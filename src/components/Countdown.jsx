9d0949cf19ee901bdc558b0e7b47bd80da3dac61
thenora <thenora@gmail.com>
2020-07-06 19:41:53 -0700

    added moment duration for mm:ss formatting

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
+import moment from "moment";
+import momentDurationFormatSetup from 'moment-duration-format';
+
+momentDurationFormatSetup(moment)
+
 
 const Countdown = ({ workTime }) => {
   const [countdown, setCountdown] = useState(workTime);
-  return <div>{countdown}</div>;
+  const formattedCountdown = moment.duration(countdown, 's').format('mm:ss');
+  return <div>{formattedCountdown}</div>;
 };
 
 export default Countdown;
