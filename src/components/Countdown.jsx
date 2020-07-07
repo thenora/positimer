b9a56df7b7a3aa67a89a5a244a736f7e107647d7
thenora <thenora@gmail.com>
2020-07-06 19:47:42 -0700

    added use effect to countdown to match worktime

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
 import moment from "moment";
 import momentDurationFormatSetup from 'moment-duration-format';
 
@@ -7,6 +7,12 @@ momentDurationFormatSetup(moment)
 
 const Countdown = ({ workTime }) => {
   const [countdown, setCountdown] = useState(workTime);
+
+  // Change countdown when workTime changes
+  useEffect(() => {
+    setCountdown(workTime)
+  }, [workTime]);
+
   const formattedCountdown = moment.duration(countdown, 's').format('mm:ss');
   return <div>{formattedCountdown}</div>;
 };
