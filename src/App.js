768b5e49db40f358d152e9b9fba27a86f20ead63
thenora <thenora@gmail.com>
2020-07-06 18:50:37 -0700

    added break to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Work from './components/Work'
+import Break from './components/Break'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Work />
+      <Break />
     </div>
   );
 }
