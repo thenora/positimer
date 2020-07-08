0ffd36025f3951e51b9d3755ac355423df1f24d8
thenora <thenora@gmail.com>
2020-07-07 18:29:16 -0700

    moved states and constants from countdown to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Work from './components/Work'
 import Break from './components/Break'
 import Countdown from './components/Countdown'
 import './App.css';
 
 function App() {
+  const [currentTimerType, setCurrentTimerType] = useState("Work");
+  const [intervalId, setIntervalId] = useState(null);
   const [workTime, setWorkTime] = useState(60 * 25); // time in seconds
   const [breakTime, setBreakTime] = useState(300);
+  const [countdown, setCountdown] = useState(workTime);
+
+  // Change countdown when workTime changes
+  useEffect(() => {
+    setCountdown(workTime);
+  }, [workTime]);
 
   const lowerWorkTimeByOneMinute = () => {
     const newWorkTime = workTime - 60;
@@ -21,6 +29,75 @@ function App() {
   const raiseWorkTimeByOneMinute = () => {
     setWorkTime(workTime + 60);
   };
+
+  const isStarted = intervalId != null;
+
+  // if countdown is zero, change work to break or break to work
+  useEffect(() => {
+    if (countdown === 0) {
+      if (currentTimerType === "Work") {
+        setCurrentTimerType("Break");
+        setCountdown(breakTime);
+      } else if (currentTimerType === "Break") {
+        setCurrentTimerType("Work");
+        setCountdown(workTime);
+      }
+    }
+  }, [breakTime, currentTimerType, workTime, countdown]);
+
+  const handleStartStopClick = () => {
+    if (isStarted) {
+      // if timer is started
+      // we want to stop and clear
+      clearInterval(intervalId);
+      setCountdown(workTime);
+      setCurrentTimerType("Work");
+      setIntervalId(null);
+    } else {
+      // if timer is stopped:
+      // lower countdown for each second
+      // 1000 ms is 1 second
+      const newIntervalId = setInterval(() => {
+        setCountdown(prevCountdown => prevCountdown - 1);
+        //   const newCountdown = prevCountdown - 1;
+        //   if (newCountdown >= 0) {
+        //     return prevCountdown - 1;
+        //   }
+
+        //   // if work, switch to break
+        //   if (currentTimerType === "Work") {
+        //     setCurrentTimerType("Break");
+        //     // set countdown to breakTime
+        //     setCountdown(breakTime);
+        //   }
+
+        //   // if break, switch to work
+        //   else if (currentTimerType === "Break") {
+        //     setCurrentTimerType("Work");
+        //     // set countdown to breakTime
+        //     setCountdown(workTime);
+        //   }
+        //   // set countdown to workTime
+        //   return prevCountdown;
+        // });
+      }, 100); // TODO reset to 1000
+      setIntervalId(newIntervalId);
+    }
+  };
+
+  const handleResetButtonClick = () => {
+    // clear the countdown interval
+
+    // set the intervalId null
+
+    // set the timertype to "Work"
+
+    // reset the workTime to 25 minutes
+
+    // reset the breakTime to 5
+
+    // reset the timer to 25 minutes (initial workTime)
+  }
 
   const lowerBreakTimeByOneMinute = () => {
     const newBreakTime = breakTime - 60;
@@ -48,6 +125,10 @@ function App() {
       <Countdown
         workTime={workTime}
         breakTime={breakTime}
+        timerLabel={currentTimerType}
+        handleStartStopClick={handleStartStopClick}
+        startStopButtonLabel={isStarted ? "Stop" : "Start"}
+        countdown={countdown}
       />
       <Break
         breakTime={breakTime}
--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,86 +1,24 @@
 import React, { useState } from "react";
 import moment from "moment";
 import momentDurationFormatSetup from "moment-duration-format";
-import { useEffect } from "react";
 
 momentDurationFormatSetup(moment);
 
-const Countdown = ({ breakTime, workTime }) => {
-  const [currentTimerType, setCurrentTimerType] = useState("Work");
-  const [intervalId, setIntervalId] = useState(null);
-  const [countdown, setCountdown] = useState(workTime);
-
-  // Change countdown when workTime changes
-  useEffect(() => {
-    setCountdown(workTime);
-  }, [workTime]);
-
-  const isStarted = intervalId != null;
-
-  // if countdown is zero, change work to break or break to work
-  useEffect(() => {
-    if (countdown === 0) {
-      if (currentTimerType === "Work") {
-        setCurrentTimerType("Break");
-        setCountdown(breakTime);
-      } else if (currentTimerType === "Break") {
-        setCurrentTimerType("Work");
-        setCountdown(workTime);
-      }
-    }
-  }, [breakTime, currentTimerType, workTime, countdown]);
-
-  const handleStartStopClick = () => {
-    if (isStarted) {
-      // if timer is started
-      // we want to stop and clear
-      clearInterval(intervalId);
-      setCountdown(workTime);
-      setCurrentTimerType("Work");
-      setIntervalId(null);
-    } else {
-      // if timer is stopped:
-      // lower countdown for each second
-      // 1000 ms is 1 second
-      const newIntervalId = setInterval(() => {
-        setCountdown(prevCountdown => prevCountdown - 1);
-        //   const newCountdown = prevCountdown - 1;
-        //   if (newCountdown >= 0) {
-        //     return prevCountdown - 1;
-        //   }
-
-        //   // if work, switch to break
-        //   if (currentTimerType === "Work") {
-        //     setCurrentTimerType("Break");
-        //     // set countdown to breakTime
-        //     setCountdown(breakTime);
-        //   }
-
-        //   // if break, switch to work
-        //   else if (currentTimerType === "Break") {
-        //     setCurrentTimerType("Work");
-        //     // set countdown to breakTime
-        //     setCountdown(workTime);
-        //   }
-        //   // set countdown to workTime
-        //   return prevCountdown;
-        // });
-      }, 100); // TODO reset to 1000
-      setIntervalId(newIntervalId);
-    }
-  };
-
+const Countdown = ({
+  handleStartStopClick,
+  timerLabel,
+  countdown,
+  startStopButtonLabel,
+}) => {
   const formattedCountdown = moment
     .duration(countdown, "s")
     .format("mm:ss", { trim: false });
   return (
     <div>
-      <p id="timer-label">{currentTimerType}</p>
+      <p id="timer-label">{timerLabel}</p>
       <p>{formattedCountdown}</p>
 
-      <button onClick={handleStartStopClick}>
-        {isStarted ? "Stop" : "Start"}
-      </button>
+      <button onClick={handleStartStopClick}>{startStopButtonLabel}</button>
     </div>
   );
 };
