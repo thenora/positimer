0a63e42043169ea90e386573d431e7cd68a6c6b5
thenora <thenora@gmail.com>
2020-07-07 17:50:13 -0700

    deployed build

---
+++ build/precache-manifest.e0b5f1dbb289816605d20d3c7817e7a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95756709791a8456c988520097197d71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c49db5000487a0e2218",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "22279d2a1d2137e2ba74",
+    "url": "/static/js/2.9402852c.chunk.js"
+  },
+  {
+    "revision": "5d7719635e368d1c17de69a19973b100",
+    "url": "/static/js/2.9402852c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c49db5000487a0e2218",
+    "url": "/static/js/main.f8c8176f.chunk.js"
+  },
+  {
+    "revision": "51efdcde318316b1bb49",
+    "url": "/static/js/runtime-main.8a196cef.js"
+  }
+]);
