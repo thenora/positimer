8d87fccf761ee6ce655edaca14d0f8f0fddb083a
thenora <thenora@gmail.com>
2020-07-10 12:34:16 -0700

    ?

---
+++ build/precache-manifest.096acd756b92f70509e3a1d6a291e935.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48d0e0d92614895263e26403047500e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48c99104560de95566e4",
+    "url": "/static/css/main.459bc3fb.chunk.css"
+  },
+  {
+    "revision": "1a8296374d8c1da147ee",
+    "url": "/static/js/2.49380118.chunk.js"
+  },
+  {
+    "revision": "5d7719635e368d1c17de69a19973b100",
+    "url": "/static/js/2.49380118.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48c99104560de95566e4",
+    "url": "/static/js/main.8bd9174e.chunk.js"
+  },
+  {
+    "revision": "51efdcde318316b1bb49",
+    "url": "/static/js/runtime-main.8a196cef.js"
+  }
+]);
