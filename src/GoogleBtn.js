ed44627e0bb6a5601a13f837b30526189780b596
thenora <thenora@gmail.com>
2020-07-20 21:56:29 -0700

    Added name to login

--- src/GoogleBtn.js
+++ src/GoogleBtn.js
@@ -3,12 +3,13 @@ import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
 
 class GoogleBtn extends Component {
-   constructor(props) {
+  constructor(props) {
     super(props);
 
     this.state = {
       isLoggedIn: false,
-      accessToken: ''
+      accessToken: '',
+      googleName: ''
     };
 
     this.login = this.login.bind(this);
@@ -17,53 +18,57 @@ class GoogleBtn extends Component {
     this.handleLogoutFailure = this.handleLogoutFailure.bind(this);
   }
 
-  login (response) {
-    if(response.accessToken){
+  login(response) {
+    if (response.accessToken) {
+      console.log(response)
       this.setState(state => ({
         isLoggedIn: true,
-        accessToken: response.accessToken
+        accessToken: response.accessToken,
+        googleName: response.profileObj.givenName
       }));
     }
   }
 
-  logout (response) {
+  logout(response) {
     this.setState(state => ({
       isLoggedIn: false,
       accessToken: ''
     }));
   }
 
-  handleLoginFailure (response) {
+  handleLoginFailure(response) {
     alert('Failed to log in')
   }
 
-  handleLogoutFailure (response) {
+  handleLogoutFailure(response) {
     alert('Failed to log out')
   }
 
   render(
   ) {
     return (
-    <div>
-      { this.state.isLoggedIn ?
-        <GoogleLogout
-          clientId={ process.env.REACT_APP_CLIENT_ID }
-          buttonText='Logout'
-          onLogoutSuccess={ this.logout }
-          onFailure={ this.handleLogoutFailure }
-        >
-        </GoogleLogout>: <GoogleLogin
-          clientId={ process.env.REACT_APP_CLIENT_ID }
-          buttonText='Login'
-          onSuccess={ this.login }
-          onFailure={ this.handleLoginFailure }
-          cookiePolicy={ 'single_host_origin' }
-          responseType='code,token'
-        />
-      }
-      {/* { this.state.accessToken ? <h5>Your Access Token: <br/><br/> { this.state.accessToken }</h5> : null } */}
-
-    </div>
+      <div>
+        {this.state.isLoggedIn ?
+          <GoogleLogout
+            clientId={process.env.REACT_APP_CLIENT_ID}
+            buttonText='Logout'
+            onLogoutSuccess={this.logout}
+            onFailure={this.handleLogoutFailure}
+          >
+          </GoogleLogout> : <GoogleLogin
+            clientId={process.env.REACT_APP_CLIENT_ID}
+            buttonText='Login'
+            onSuccess={this.login}
+            onFailure={this.handleLoginFailure}
+            cookiePolicy={'single_host_origin'}
+            responseType='code,token'
+          />
+        }
+        <div>
+          {this.state.googleName ? <p>Hiya { this.state.googleName }!</p> : null }
+          {/* { this.state.accessToken ? <h5>Your Access Token: <br/><br/> { this.state.accessToken }</h5> : null } */}
+        </div>
+      </div>
     )
   }
 }
