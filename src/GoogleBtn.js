31c45720cc27dd009665b9300f8ddee4eb4ab970
thenora <thenora@gmail.com>
2020-07-21 22:25:57 -0700

    moved google btn props up to nav

--- src/GoogleBtn.js
+++ src/GoogleBtn.js
@@ -3,69 +3,29 @@ import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
 
 class GoogleBtn extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isLoggedIn: false,
-      accessToken: '',
-      googleName: ''
-    };
-
-    this.login = this.login.bind(this);
-    this.handleLoginFailure = this.handleLoginFailure.bind(this);
-    this.logout = this.logout.bind(this);
-    this.handleLogoutFailure = this.handleLogoutFailure.bind(this);
-  }
-
-  login(response) {
-    if (response.accessToken) {
-      console.log(response)
-      this.setState(state => ({
-        isLoggedIn: true,
-        accessToken: response.accessToken,
-        googleName: response.profileObj.givenName
-      }));
-    }
-  }
-
-  logout(response) {
-    this.setState(state => ({
-      isLoggedIn: false,
-      accessToken: ''
-    }));
-  }
-
-  handleLoginFailure(response) {
-    alert('Failed to log in')
-  }
-
-  handleLogoutFailure(response) {
-    alert('Failed to log out')
-  }
-
+  
   render(
   ) {
     return (
       <div>
-        {this.state.isLoggedIn ?
+        {(this.props.isLoggedIn && this.props.googleName) ? <p>Hiya { this.props.googleName }!</p> : null }
+        {this.props.isLoggedIn ?
           <GoogleLogout
             clientId={process.env.REACT_APP_CLIENT_ID}
             buttonText='Logout'
-            onLogoutSuccess={this.logout}
+            onLogoutSuccess={this.props.logout}
             onFailure={this.handleLogoutFailure}
           >
           </GoogleLogout> : <GoogleLogin
             clientId={process.env.REACT_APP_CLIENT_ID}
             buttonText='Login'
-            onSuccess={this.login}
+            onSuccess={this.props.login}
             onFailure={this.handleLoginFailure}
             cookiePolicy={'single_host_origin'}
             responseType='code,token'
           />
         }
         <div>
-          {(this.state.isLoggedIn && this.state.googleName) ? <p>Hiya { this.state.googleName }!</p> : null }
           {/* { this.state.accessToken ? <h5>Your Access Token: <br/><br/> { this.state.accessToken }</h5> : null } */}
         </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,48 @@ import React, { Component } from 'react';
 import GoogleBtn from '../GoogleBtn';
 
 export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoggedIn: false,
+      accessToken: '',
+      googleName: ''
+    };
+
+    this.login = this.login.bind(this);
+    this.handleLoginFailure = this.handleLoginFailure.bind(this);
+    this.logout = this.logout.bind(this);
+    this.handleLogoutFailure = this.handleLogoutFailure.bind(this);
+  }
+
+  login(response) {
+    if (response.accessToken) {
+      console.log(response)
+      this.setState(state => ({
+        isLoggedIn: true,
+        accessToken: response.accessToken,
+        googleName: response.profileObj.givenName
+      }));
+    }
+  }
+
+  logout(response) {
+    this.setState(state => ({
+      isLoggedIn: false,
+      accessToken: ''
+    }));
+  }
+
+  handleLoginFailure(response) {
+    alert('Failed to log in')
+  }
+
+  handleLogoutFailure(response) {
+    alert('Failed to log out')
+  }
+
+
   render() {
     return (
       <nav className="navbar" role="navigation" aria-label="main navigation">
@@ -29,9 +71,13 @@ export default class Navbar extends Component {
 
           <div className="navbar-end">
             <div className="navbar-item">
-              <div className="buttons">
-                <GoogleBtn />
-              </div>
+                <GoogleBtn
+                isLoggedIn={this.state.isLoggedIn}
+                googleName={this.state.googleName}
+                login={this.login}
+                logout={this.logout}
+                accessToken={this.state.accessToken}
+                />
             </div>
           </div>
         </div>
