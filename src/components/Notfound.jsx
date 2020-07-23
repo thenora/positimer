4914623ce6878cf5968f29f2b9c1eeb47d1db76a
thenora <thenora@gmail.com>
2020-07-22 19:37:20 -0700

    added more to notfound page

--- src/components/Notfound.jsx
+++ src/components/Notfound.jsx
@@ -3,8 +3,11 @@ import React from "react";
 const Notfound = () => (
   <div className="container">
     <div className="section">
-    <h1>Not Found</h1>
-    <p className="clock">04:04</p>
+      <div className="content">
+        <h1>Not Found</h1>
+        <p className="clock">04:04</p>
+        <p className="text text-is-centered">Oops. Looks like it's time to try another page.</p>
+      </div>
     </div>
   </div>
 );
