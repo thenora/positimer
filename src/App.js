cd7d4b480d2c39d7f0b77afa7d5df98cfe647680
thenora <thenora@gmail.com>
2020-07-22 21:57:24 -0700

    cleaned up unused items

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Work from './components/Work';
 import Home from './components/Home';
-import Break from './components/Break';
-import Stats from './components/Stats';
 import About from './components/About';
-import Countdown from './components/Countdown';
 import Quotes from './components/Quotes';
-import ShowQuote from './components/ShowQuote';
 import QuoteAdmin from './components/QuoteAdmin';
 import PrivacyPolicy from './components/PrivacyPolicy';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Notfound from './components/Notfound';
-import * as workerInterval from 'worker-interval';
 import './App.css';
 
 function App() {
@@ -25,7 +19,6 @@ function App() {
         <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
-
           <Route exact path="/about" component={About} />
           <Route exact path="/quotes" component={Quotes} />
           <Route exact path="/admin" component={QuoteAdmin} />
